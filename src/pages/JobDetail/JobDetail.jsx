4243ef707c99b36a18028093d13679eda6f2c3af
PhamNhatAnh2003 <anhpham090423@gmail.com>
2025-05-15 20:20:06 +0700

    jobDetail

--- src/pages/JobDetail/JobDetail.jsx
+++ src/pages/JobDetail/JobDetail.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState} from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -8,41 +8,15 @@ import PeopleIcon from "@mui/icons-material/People";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import companyLogo from "../../assets/company-logo.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import "./JobDetail.css";
 import Header from "../../components/Header";
-
-const fakeJob = {
-  id:1,
-  title: "Gia sư Toán lớp 10",
-  type: "Part-time",
-  category: "Gia sư",
-  address: "Ngõ 265, Lương Thế Vinh, Nam Từ Liêm, Trung Văn, Hà Nội",
-  salary: "200.000đ/giờ",
-  experienceRequired: "1 năm",
-  company: {
-    avatar: null,
-    name: "G8 - Onschool",
-    location: "Hà Nội",
-    people: 100,
-    addresses: "74 ngõ Xã Đàn 2, Nam Đồng, Đống Đa, Hà Nội",
-    Job: "Giảng dạy",
-  },
-  quantity: 2,
-  schedule: "Thứ 2, 4, 6 (18:00 - 20:00)",
-  startDate: "2025-06-01",
-  endDate: "2025-08-31",
-  description: [
-    "Dạy kèm Toán cho học sinh lớp 10.",
-    "Ôn tập kiến thức căn bản và nâng cao.",
-    "Báo cáo kết quả học tập mỗi tháng.",
-  ],
-};
-
 
 const JobDetail = () => {
   const { id } = useParams();
   const [job, setJob] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchJob = async () => {
@@ -68,7 +42,10 @@ const JobDetail = () => {
       <div className="page-content">
         <div className="job-container">
           <div className="newdiv">
-            <h1 className="job-title">{job.title}</h1>
+            <h1 onClick={() => navigate(-1)} className="job-title">
+              <FontAwesomeIcon icon={faChevronLeft} />
+              {job.title}
+            </h1>
             <div className="badges">
               <span className="badge type">{job.jobType}</span>
               <span className="badge category">{job.category}</span>
@@ -83,7 +60,7 @@ const JobDetail = () => {
                   <LocationOnIcon /> {job.address}
                 </li>
                 <li>
-                  <MonetizationOnIcon /> {job.salary}
+                  <MonetizationOnIcon /> {job.salary}/{job.salaryUnit}
                 </li>
                 <li>
                   <WorkHistoryIcon />{" "}
